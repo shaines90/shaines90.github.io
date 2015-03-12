ddf7bb43b6707b61a936002fec771f44606a7022
Sophia Haines <sophia.haines@gmail.com>
2015-03-12 11:41:21 +0800

    php contact form

---
+++ contact-us.php
@@ -0,0 +1,36 @@
+<?php
+
+// DISPLAY ERRORS
+//ini_set('display_errors',1);
+//ini_set('display_startup_errors',1);
+//error_reporting(-1);
+
+// GRAB ALL VARIABLES
+$fullName = $_POST['fullName'];
+$contactEmail = $_POST['contactEmail'];
+$content = $_POST['content'];
+
+// SEND OUT THE E-MAIL WITH THAT PDF
+require_once('PHPMailer/class.phpmailer.php');
+$mail = new PHPMailer(); // defaults to using php "mail()"
+$body =
+      "Name: $fullName<BR><BR>".
+      "Email: $contactEmail <BR><BR>".
+      "Message: $content <BR>"; // Here is the body content
+
+$mail->AddReplyTo("hello@talkpush.com","Talkpush System");
+$mail->SetFrom('hello@talkpush.com', 'Talkpush System');
+
+$mail->AddAddress("sophia.haines@talkpush.com","LPUtalk");
+$mail->Subject    = "LPU Contact";
+$mail->MsgHTML($body);
+if(!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+//  echo "Message sent!";
+}
+
+// Redirect to next page
+header('Location: thanks.html');
+?>
+
